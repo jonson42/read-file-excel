dc9a0ee7bce417aaae1f58cbef25d2283a2ed0d8
Nghiem Trinh Quoc <nghiemtq@unit.com.vn>
2021-06-13 19:30:46 +0700

    update code

---
+++ ReadFileExcel/ReadFileExcel/Excell.cs
@@ -0,0 +1,47 @@
+﻿using ExcelDataReader;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+
+namespace WindowsFormsControlLibrary1
+{
+    public class Excell
+    {
+        private string _path { get; set; }
+        private List<object> _list { get; set; }
+        public Excell(string path)
+        {
+            _path = path;
+            _list = new List<object>();
+        }
+        public List<object> ReadFile(string sheetName)
+        {
+            using (var stream = System.IO.File.Open(_path,FileMode.Open, FileAccess.Read))
+            {
+
+                IExcelDataReader excelDataReader = ExcelDataReader.ExcelReaderFactory.CreateReader(stream);
+
+                var conf = new ExcelDataSetConfiguration()
+                {
+                    ConfigureDataTable = a => new ExcelDataTableConfiguration
+                    {
+                        UseHeaderRow = true
+                    }
+                };
+
+                DataSet dataSet = excelDataReader.AsDataSet(conf);
+                DataRowCollection row = dataSet.Tables[sheetName].Rows;
+                List<object> rowDataList = null;
+                List<object> allRowsList = new List<object>();
+                foreach (DataRow item in row)
+                {
+                    rowDataList = item.ItemArray.ToList();
+                    allRowsList.Add(rowDataList);
+                }
+                _list = allRowsList;
+                return allRowsList;
+            }
+        }
+    }
+}
